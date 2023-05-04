69db1f57967f50204a79739c5a58c5fa4e2fb884
Bijendra Raut <bijendraghvm@gmail.com>
2023-05-04 20:42:14 +0530

    comment removed

--- isTraingle.js
+++ isTraingle.js
@@ -4,11 +4,6 @@ const outputxt = document.querySelector("#output-text");
 
 btnCheck.addEventListener("click", isTraingle);
 
-// function findTraingle(angle1, angle2, angle3) {
-//   const sum = angle1 + angle2 + angle3;
-//   return sum;
-// }
-
 function isTraingle() {
   const sum =
     Number(angleInput[0].value) +
