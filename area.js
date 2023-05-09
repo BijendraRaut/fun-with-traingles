38ba2a22e416a7d2684f51209f07c70fdb938c76
Bijendra Raut <bijendraghvm@gmail.com>
2023-05-09 23:59:48 +0530

    js work done

--- area.js
+++ area.js
@@ -1,1 +1,10 @@
-alert("working");
+const sides = document.querySelectorAll(".sides");
+const calculateBtn = document.querySelector("#check-area");
+const outputEl = document.querySelector("#output");
+
+function calculateArea(a, b) {
+  const area = (a * b) / 2;
+  console.log(area);
+}
+
+calculateBtn.addEventListener("click", calculateArea);
