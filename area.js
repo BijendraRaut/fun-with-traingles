772cfbc66bf0f2f81007e364b2f17bf39edfe8d2
Bijendra Raut <bijendraghvm@gmail.com>
2023-05-10 00:22:39 +0530

    bug fixed

--- area.js
+++ area.js
@@ -2,9 +2,19 @@ const sides = document.querySelectorAll(".sides");
 const calculateBtn = document.querySelector("#check-area");
 const outputEl = document.querySelector("#output");
 
-function calculateArea(a, b) {
-  const area = (a * b) / 2;
-  console.log(area);
+function calculateBaseAndHeight(b, h) {
+  const baseandHeight = b * h;
+  return baseandHeight;
+}
+
+function calculateArea() {
+  const baseandHeight = calculateBaseAndHeight(
+    Number(sides[0].value),
+    Number(sides[1].value)
+  );
+  //console.log(baseandHeight);
+  const area = baseandHeight / 2;
+  outputEl.innerText = `The area of traingle is ${area} cm²`;
 }
 
 calculateBtn.addEventListener("click", calculateArea);
